b5fca2542562eea3dab01014ee98892719516233
Muhammad Usama <89479745+rmusamam@users.noreply.github.com>
2022-06-04 20:34:02 +0500

    all project done in JQuery

--- js/app.js
+++ js/app.js
@@ -1,40 +1,44 @@
 // show cart
 
 (function () {
-  const cartInfo = document.getElementById("cart-info");
+  //const cartInfo = document.getElementById("cart-info");
+  const cartInfo = $("#cart-info");
 
-  const cart = document.getElementById("cart");
+  //const cart = document.getElementById("cart");
+  const cart = $("#cart");
 
-  cartInfo.addEventListener("click", function () {
-    cart.classList.toggle("show-cart");
+  cartInfo.click(function () {
+    cart.toggleClass("show-cart");
   });
 })();
 
 //add items in to cart
 
 (function () {
-  const cartBtn = document.querySelectorAll(".store-item-icon");
+  //const cartBtn = document.querySelectorAll(".store-item-icon");
+  let cartBin = $(".store-item-icon");
 
-  cartBtn.forEach(function (btn) {
-    btn.addEventListener("click", function (event) {
+  //cartBtn.forEach(function (btn)
+  cartBin.each(function (btn) {
+    //btn.addEventListener("click", function (event)
+    $(this).click(function (event) {
       //console.log(event.target.parentElement.classList)
-      if (event.target.parentElement.classList.contains("store-item-icon")) {
-        let fullPath = event.target.parentElement.previousElementSibling.src;
+      // if (event.target.parentElement.classList.contains("store-item-icon")) {
+      if ($(this).hasClass("store-item-icon")) {
+        //let fullPath = event.target.parentElement.previousElementSibling.src;
+        let fullPath = $(this).prev().attr('src');
+        //let pos = fullPath.indexOf("img") + 3;
         let pos = fullPath.indexOf("img") + 3;
-        //console.log(pos);
         let partPath = fullPath.slice(pos);
-
         const item = {};
         item.img = `img-cart${partPath}`;
 
-        let name =
-          event.target.parentElement.parentElement.nextElementSibling
-            .children[0].children[0].textContent;
+        let name = $(this).parent().next().children(':first').children(':first').text();
 
         let price =
-          event.target.parentElement.parentElement.nextElementSibling
-            .children[0].children[1].textContent;
-
+          //event.target.parent().parent().next().children[0].children[1].textContent;
+          $(this).parent().next().children(':first').children(':first').text();
+          
         let finalPrice = price.slice(1).trim();
 
         item.name = name;
@@ -43,13 +47,7 @@
         // console.log(item);
 
         const cartItem = document.createElement("div");
-        cartItem.classList.add(
-          "cart-Item",
-          "d-flex",
-          "justify-content-between",
-          "text-capitalize",
-          "my-3"
-        );
+        $(cartItem).addClass("cart-Item d-flex justify-content-between text-capitalize my-3");
 
         cartItem.innerHTML = ` 
        <img src="${item.img}" class="img-fluid 
@@ -68,10 +66,11 @@
 
         // select Cart
 
-        const cart = document.getElementById("cart");
-        const total = document.querySelector(".cart-total-container");
+        const cart = $("#cart");
+        const total = $(".cart-total-container");
 
-        cart.insertBefore(cartItem, total);
+        total.before(cartItem)
+        // cart.insertBefore(cartItem, total);
         alert("Item added to cart");
 
         showTotals();
@@ -82,8 +81,10 @@
   //show total
   function showTotals() {
     const total = [];
-    const items = document.querySelectorAll(".cart-item-price");
-    items.forEach(function (item) {
+    //debugger;
+  
+    const items = $(".cart-item-price");
+    items.each(function (item) {
       total.push(parseFloat(item.textContent));
     });
 
@@ -94,32 +95,38 @@
     const finalMoney = totalMoney.toFixed(2);
     // console.log(finalMoney)
 
-    document.getElementById("cart-total").textContent = finalMoney;
-    document.querySelector(".item-total").textContent = finalMoney;
-    document.getElementById("item-count").textContent = total.length;
+    $("#cart-total").text = finalMoney;
+    $(".item-total").text = finalMoney;
+    $("#item-count").text = total.length;
   }
 })();
 
 // clear Cart
 
 (function () {
-  let clr = document.getElementById("clear-cart");
+  let clr = $("#clear-cart");
 
-  clr.addEventListener("click", function () {
-    document.querySelector(".item-total").textContent = 0;
-    document.querySelector("#cart-total").textContent = 0;
-    let clearCartItem = document.getElementsByClassName("cart-Item"); // HTMLCollection
+  clr.on("click", function () {
+    $(".item-total").text = 0;
+    $("#cart-total").text = 0;
+    let clearCartItem = $(".cart-Item"); // HTMLCollection
 
-    Array.from(clearCartItem).forEach(function(el) {
-      el.remove()
+    
+
+    clearCartItem.each(function(){
+      $(this).remove()
     })
+
+    // Array.from(clearCartItem).forEach(function (el) {
+    //   el.remove();
+    // });
   });
 })();
 
-(function(){
-  let bin=document.getElementById('cart-item-remove')
-  // console.log(bin)
-  bin.addEventListener('click',function(event){
-   console.log(event.target.parentElement.parentElement) 
-  })
-})();
+// (function () {
+//   let bin = document.getElementById("cart-item-remove");
+//   // console.log(bin)
+//   bin.addEventListener("click", function (event) {
+//     console.log(event.target.parentElement.parentElement);
+//   });
+// })();
